3750e241543cb39c69dd1d380e572f1b0b453e45
GluKosAlex <glukosalex@gmail.com>
2024-02-11 22:28:37 +0700

    feat(app): add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,21 @@
 function Footer() {
-  return <div>Footer</div>;
+  const date = new Date();
+
+  return (
+    <footer className='flex min-h-10 bg-slate-700 text-white'>
+      <div className='flex justify-between w-full max-w-5xl p-4 mx-auto'>
+        <a
+          className='text-bold'
+          href='https://github.com/GluKosAlex'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          © Konstantin Glushenkov
+        </a>
+
+        <span>{date.getFullYear()}</span>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
